f823d53623302e3235d2836fb599bd5de78546a7
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-24 19:29:27 +0700

    mouse-trail code

--- src/app/components/mouse.trail.tsx
+++ src/app/components/mouse.trail.tsx
@@ -2,6 +2,16 @@ import React from "react";
 
 type Props = {};
 
-function MouseTrail({}: Props) {}
+function MouseTrail({}: Props) {
+	const trailIndex = Array(10).fill(null);
+
+	return (
+		<>
+			{trailIndex.map((_, index) => (
+				<div className={"mouse-trails trail-" + index} key={index}></div>
+			))}
+		</>
+	);
+}
 
 export default MouseTrail;
