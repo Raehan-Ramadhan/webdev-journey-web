94c9bb1a31beb82be8af6ff621c6dbe7f74f842d
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-24 19:28:56 +0700

    mouse-trailer init

---
+++ src/app/components/mouse.trail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+function MouseTrail({}: Props) {}
+
+export default MouseTrail;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import Header from "./components/header";
+import MouseTrail from "./components/mouse.trail";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
+			<MouseTrail />
 		</>
 	);
 }
