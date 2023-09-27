875005718f11b09f778e33b597e18c19673246ae
GrayCatTheOne <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-27 22:06:19 +0700

    Scrollbar initialization

---
+++ src/app/components/scrollbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Scrollbar from "smooth-scrollbar";
+import { useEffect } from "react";
+
+function MyScrollBar() {
+	useEffect(() => {
+		Scrollbar.init(document.body);
+		return Scrollbar.destroy(document.body);
+	});
+	return null;
+}
+
+export default MyScrollBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import styles from "./page.module.css";
 import Header from "./components/header";
 import MouseTrail from "./components/mouse.trail";
 import Gallery from "./components/gallery";
+import MyScrollBar from "./components/scrollbar";
 
 export default function Home() {
 	return (
 		<>
+			<MyScrollBar />
 			<Gallery />
 			<Header />
 			<MouseTrail />
