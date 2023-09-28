949bc8eb20884884c45f70f4a4e64b73c809b7bd
GrayCatTheOne <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-28 16:56:36 +0700

    moved scrollbar from app\components into src\app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import Navigation from "./components/navigation";
 import MouseTrail from "./components/mouse.trail";
 import Gallery from "./components/gallery";
-import MyScrollBar from "./components/scrollbar";
+import MyScrollBar from "./scrollbar";
 
 export default function Home() {
 	return (
