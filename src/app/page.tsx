c7775297cdd27e8c43aafb2457bfa9a85ae738de
GrayCatTheOne <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-10-06 23:23:42 +0700

    isDesktop changed to isMobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navigation from "./components/navigation";
 import MouseTrail from "./components/mouse.trail";
 import MyScrollBar from "./scrollbar";
 import Gallery from "./components/gallery";
-import { isDesktop } from "react-device-detect";
+import { isMobile } from "react-device-detect";
 
 export default function Home() {
 	const imagesCSSJourney: string[] = [
@@ -52,7 +52,7 @@ export default function Home() {
 			<Gallery imagesSource={imagesCSSJourney} details={detailsCSSJourney} />
 			<div style={{ height: "100vh", width: "100vw" }}></div>
 			<Navigation />
-			{isDesktop && <MouseTrail />}
+			{!isMobile && <MouseTrail />}
 		</>
 	);
 }
