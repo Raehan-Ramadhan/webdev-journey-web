b01b98048ffee0cc68382618df47810886bfbab0
GrayCatTheOne <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-10-14 12:16:02 +0700

    Navigation in Blogs

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,70 +1,76 @@
 import "./blogs.css";
+import Navigation from "@/app/components/navigation";
+
 function blogs() {
 	return (
 		<>
-			<h1>Childfree</h1>
-			<p>&nbsp;</p>
-			<p>
-				&emsp; In the last two decades, researchers have been concerned with the
-				growth of the child-free phenomenon in many first-world nations with
-				multiple dimensions of modernity (Bhambhani, 2020). A lot of theories
-				and opinions have emerged to address this phenomenon.
-			</p>
-			<p>&nbsp;</p>
-			<p>
-				&emsp; The childfree phenomenon has divided people&apos;s views into two
-				groups. First, 60.8% of the populace denies the legality of the
-				child-free phenomenon in the middle of society. Second, the other 39.8%
-				of the people are willing to be more open and agree with the application
-				of childfree in their lives.
-			</p>
-			<p>&nbsp;</p>
-			<p>
-				&emsp; Researchers&apos; findings have shown that the group who agreed
-				with the application of childfree have a few reasons, some of them are:
-			</p>
-			<p>&nbsp;</p>
-			<ol>
-				<li>
-					It is in everyone&apos;s right. Choice, personal decisions, and
-					consequences have become that person&apos;s responsibility.
-				</li>
-				<li>They have logical reasoning.</li>
-				<li>
-					Plenty of older people are not ready to have a child, whether in the
-					aspect of readied to teach or to finance.
-				</li>
-				<li>To have a child is not a responsibility.</li>
-				<li>Not everyone can create a child.</li>
-				<li>
-					Many children have become the target of anger, failure, and
-					disappointment their parents have felt.
-				</li>
-				<li>
-					Some parent regards their children as a form of long-term investment,
-					which is when the child has become an adult and could work to help
-					finance daily and household needs.
-				</li>
-				<li>
-					Children have the right to have a parent who is qualified and
-					competent.
-				</li>
-			</ol>
-			<p>&nbsp;</p>
-			<p>
-				&emsp; Meanwhile, the contrasting groups have opposite opinions. Some of
-				them are:
-			</p>
-			<p>&nbsp;</p>
-			<ol>
-				<li>
-					Because it abolishes women&apos;s nature (to conceive, feed, and give
-					birth).
-				</li>
-				<li>Child-free could destroy the generation chain.</li>
-				<li>Children are the gift from God.</li>
-				<li>It is not following the law of Indonesia.</li>
-			</ol>
+			<Navigation />
+			<div className="blog-container">
+				<h1>Childfree</h1>
+				<p>&nbsp;</p>
+				<p>
+					&emsp; In the last two decades, researchers have been concerned with
+					the growth of the child-free phenomenon in many first-world nations
+					with multiple dimensions of modernity (Bhambhani, 2020). A lot of
+					theories and opinions have emerged to address this phenomenon.
+				</p>
+				<p>&nbsp;</p>
+				<p>
+					&emsp; The childfree phenomenon has divided people&apos;s views into
+					two groups. First, 60.8% of the populace denies the legality of the
+					child-free phenomenon in the middle of society. Second, the other
+					39.8% of the people are willing to be more open and agree with the
+					application of childfree in their lives.
+				</p>
+				<p>&nbsp;</p>
+				<p>
+					&emsp; Researchers&apos; findings have shown that the group who agreed
+					with the application of childfree have a few reasons, some of them
+					are:
+				</p>
+				<p>&nbsp;</p>
+				<ol>
+					<li>
+						It is in everyone&apos;s right. Choice, personal decisions, and
+						consequences have become that person&apos;s responsibility.
+					</li>
+					<li>They have logical reasoning.</li>
+					<li>
+						Plenty of older people are not ready to have a child, whether in the
+						aspect of readied to teach or to finance.
+					</li>
+					<li>To have a child is not a responsibility.</li>
+					<li>Not everyone can create a child.</li>
+					<li>
+						Many children have become the target of anger, failure, and
+						disappointment their parents have felt.
+					</li>
+					<li>
+						Some parent regards their children as a form of long-term
+						investment, which is when the child has become an adult and could
+						work to help finance daily and household needs.
+					</li>
+					<li>
+						Children have the right to have a parent who is qualified and
+						competent.
+					</li>
+				</ol>
+				<p>&nbsp;</p>
+				<p>
+					&emsp; Meanwhile, the contrasting groups have opposite opinions. Some
+					of them are:
+				</p>
+				<p>&nbsp;</p>
+				<ol>
+					<li>
+						Because it abolishes women&apos;s nature (to conceive, feed, and
+						give birth).
+					</li>
+					<li>Child-free could destroy the generation chain.</li>
+					<li>Children are the gift from God.</li>
+					<li>It is not following the law of Indonesia.</li>
+				</ol>
+			</div>
 		</>
 	);
 }
