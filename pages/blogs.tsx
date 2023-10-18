5034121ae5017b5eb44c2fc33ac7de9c9a3800a8
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-10-18 13:47:05 +0700

    blog text update

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -7,28 +7,35 @@ function blogs() {
 			<Navigation />
 			<div className="blog-container">
 				<h1>Childfree</h1>
-				<p>&nbsp;</p>
 				<p>
-					&emsp; In the last two decades, researchers have been concerned with
-					the growth of the child-free phenomenon in many first-world nations
-					with multiple dimensions of modernity (Bhambhani, 2020). A lot of
-					theories and opinions have emerged to address this phenomenon.
+					<br />
 				</p>
-				<p>&nbsp;</p>
 				<p>
-					&emsp; The childfree phenomenon has divided people&apos;s views into
-					two groups. First, 60.8% of the populace denies the legality of the
+					{" "}
+					In the last two decades, researchers have been concerned with the
+					growth of the child-free phenomenon in many first-world nations with
+					multiple dimensions of modernity (Bhambhani, 2020). A lot of theories
+					and opinions have emerged to address this phenomenon.
+				</p>
+				<p>
+					<br />
+				</p>
+				<p>
+					{" "}
+					The childfree phenomenon has divided people&apos;s views into two
+					groups. First, 60.8% of the populace denies the legality of the
 					child-free phenomenon in the middle of society. Second, the other
 					39.8% of the people are willing to be more open and agree with the
 					application of childfree in their lives.
 				</p>
-				<p>&nbsp;</p>
 				<p>
-					&emsp; Researchers&apos; findings have shown that the group who agreed
-					with the application of childfree have a few reasons, some of them
-					are:
+					<br />
 				</p>
-				<p>&nbsp;</p>
+				<p>
+					{" "}
+					Researchers&apos; findings have shown that the group who agreed with
+					the application of childfree have a few reasons, some of them are:
+				</p>
 				<ol>
 					<li>
 						It is in everyone&apos;s right. Choice, personal decisions, and
@@ -55,20 +62,41 @@ function blogs() {
 						competent.
 					</li>
 				</ol>
-				<p>&nbsp;</p>
 				<p>
-					&emsp; Meanwhile, the contrasting groups have opposite opinions. Some
-					of them are:
+					{" "}
+					Meanwhile, the contrasting groups have opposite opinions. Some of them
+					are:
 				</p>
-				<p>&nbsp;</p>
 				<ol>
 					<li>
-						Because it abolishes women&apos;s nature (to conceive, feed, and
-						give birth).
+						Because it abolishes women&apos;s nature to conceive, feed, and give
+						birth.
 					</li>
 					<li>Child-free could destroy the generation chain.</li>
 					<li>Children are the gift from God.</li>
 					<li>It is not following the law of Indonesia.</li>
+					<li>
+						Because if no factor endangers the mother or the child, for example,
+						in health and psychology, perhaps the childfree is just a matter of
+						the partners&apos; selfishness.
+					</li>
+					<li>
+						Because having a child is part of human nature, furthermore from a
+						social aspect, having a child is essential because we can&apos;t
+						live alone.
+					</li>
+				</ol>
+				<p>There is also a group with neutral beliefs. Their statements are:</p>
+				<ol>
+					<li>
+						&quot;The truth is, I am in a position of agreeing and
+						disagreeing.&quot;
+					</li>
+					<li>
+						&quot;I couldn&apos;t decisively say agree or disagree because
+						plenty of other things need to be considered first before
+						deciding.&quot;
+					</li>
 				</ol>
 			</div>
 		</>
